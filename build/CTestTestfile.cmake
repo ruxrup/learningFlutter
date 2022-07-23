7b321f323380ab74c446e2ec84efd0f0e47afc83
ruxrup <dakshsrivastava2@gmail.com>
2022-07-23 08:20:37 +0530

    CMakeList.txt added

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dakshsrivastava/ONEDRIVE/Flutter Projects/learningFlutter
+# Build directory: /home/dakshsrivastava/ONEDRIVE/Flutter Projects/learningFlutter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
