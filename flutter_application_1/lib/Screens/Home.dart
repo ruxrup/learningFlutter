da21b4cc693eb55fdf4246b8ba1c667f291e6975
ruxrup <dakshsrivastava2@gmail.com>
2022-07-23 10:35:27 +0530

    local data json created

--- flutter_application_1/lib/Screens/Home.dart
+++ flutter_application_1/lib/Screens/Home.dart
@@ -1,13 +1,37 @@
 import 'dart:ui';
-
+import 'package:flutter/services.dart';
 import "package:flutter/material.dart";
+import 'dart:convert';
 import 'package:flutter_application_1/socials/catalog.dart';
 import 'package:flutter_application_1/widgets/social_widget.dart';
 
 import '../widgets/drawer.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final String t = "hello world";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadSites();
+  }
+
+  loadSites() async {
+    var importedJson = await rootBundle.loadString("assets/docs/sites.json");
+    var decodedJson = jsonDecode(importedJson);
+    var listJson = decodedJson["sites"];
+    socials_list = List.from(listJson)
+        .map<socials>((social) => socials.fromMap(social))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- flutter_application_1/lib/socials/catalog.dart
+++ flutter_application_1/lib/socials/catalog.dart
@@ -4,22 +4,20 @@ class socials {
   final String url;
 
   socials({required this.name, required this.imgurl, required this.url});
+
+  factory socials.fromMap(Map<String, dynamic> map) {
+    return socials(
+      name: map["name"],
+      imgurl: map["imgurl"],
+      url: map["url"],
+    );
+  }
+
+  toList() => {
+        "name": name,
+        "imgurl": imgurl,
+        "url": url,
+      };
 }
 
-final socials_list = [
-  socials(
-      name: "GitHub",
-      imgurl:
-          "https://th.bing.com/th/id/OIP.8SVgggxQcO5L6Dw_61ac4QHaEK?w=305&h=180&c=7&r=0&o=5&pid=1.7",
-      url: "https://github.com/"),
-  socials(
-      name: "LinkedIn",
-      imgurl:
-          "https://th.bing.com/th/id/R.0af7abc6bfa537fdd08ed300a8915063?rik=enRbSeN7x3qVMA&riu=http%3a%2f%2fwww.texber.com%2fwp-content%2fuploads%2f2020%2f04%2flogo-Linkedin-1.png&ehk=5ZGIe6g1MbAZmO6wVVi%2bId1H21GAyUS2tTZ55NDndXQ%3d&risl=&pid=ImgRaw&r=0",
-      url: "https://linkedin.com/"),
-  socials(
-      name: "Instagram",
-      imgurl:
-          "https://th.bing.com/th/id/OIP.50GW0WB6bWlZGRb0KFy47AHaEK?w=332&h=186&c=7&r=0&o=5&pid=1.7",
-      url: "https://instagram.com/"),
-];
+List<socials> socials_list = [];
